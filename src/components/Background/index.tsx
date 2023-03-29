ab7e9a757cf719e70daaf6c4105ab1f18fe9a5c2
Julio Levi <jlevicarvalho@gmail.com>
2023-03-29 18:31:34 -0300

    fix: bug with grid animation

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,7 +1,7 @@
 export const Background = () => {
   return (
     <div className="absolute z-0 left-0 top-0 bg-[url('/grid-pattern.svg')] animate-grid h-full w-full">
-      <div className="bg-[url('/waves.svg')] bg-bottom bg-no-repeat w-full h-[100%] " />
+      <div className="bg-[url('/waves.svg')] bg-bottom bg-no-repeat w-full h-[102%] " />
     </div>
   );
 };

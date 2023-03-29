5e4461d1bfe6b994ca5df70421d2e300c723447f
Julio Levi <jlevicarvalho@gmail.com>
2023-03-29 20:02:41 -0300

    feat: added links to projects

--- src/components/ProjectsSections/index.tsx
+++ src/components/ProjectsSections/index.tsx
@@ -1,10 +1,14 @@
+import GithubSvg from "@/svg/github";
+import LinkSvg from "@/svg/link";
 import Image from "next/image";
+import Link from "next/link";
 const POSTS = [
   {
     id: 1,
     title: "Blue Marble",
     tags: ["Next.js", "Three.js"],
     image: "/project--bm-landing-page.jpg",
+    preview: "https://bluemarble.com.br",
   },
 
   {
@@ -12,6 +16,8 @@ const POSTS = [
     title: "Ignews",
     tags: ["Next.js", "Stripe", "Prismic"],
     image: "/project--ignews.jpg",
+    github: "https://github.com/asleeplevi/ignews",
+    preview: "https://ignews-two-rouge.vercel.app/",
   },
 ];
 
@@ -19,11 +25,11 @@ export const ProjectsSections = () => {
   return (
     <div className="container max-w-3xl px-4">
       <h4 className="text-sm font-bold text-grey">Meus projetos</h4>
-      <ul className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-8">
+      <ul className=" mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-8">
         {POSTS.map((post) => (
           <li
             key={post.id}
-            className="w-full h-[260px] bg-grey-500 rounded-lg relative overflow-hidden"
+            className="group w-full h-[260px] bg-grey-500 rounded-lg relative overflow-hidden"
           >
             <Image
               src={post.image}
@@ -44,6 +50,27 @@ export const ProjectsSections = () => {
                 ))}
               </ul>
             </footer>
+
+            <div className="flex opacity-0 transition-all group-hover:opacity-100 absolute w-full h-full bg-black/50  justify-center items-center gap-2">
+              {post.github && (
+                <Link
+                  href={post.github}
+                  target="_blank"
+                  className="bg-white/20 rounded-full flex items-center justify-center w-[45px] h-[45px]"
+                >
+                  <GithubSvg />
+                </Link>
+              )}
+              {post.preview && (
+                <Link
+                  href={post.preview}
+                  target="_blank"
+                  className="bg-white/20 rounded-full flex items-center justify-center w-[45px] h-[45px]"
+                >
+                  <LinkSvg width={25} height={25} />
+                </Link>
+              )}
+            </div>
           </li>
         ))}
       </ul>

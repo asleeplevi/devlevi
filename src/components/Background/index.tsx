1764aea3c3fbe2774fe65c3b9ffc188d5a7bc811
Levi <jlevicarvalho@gmail.com>
2023-10-09 22:35:28 -0300

    Simplified layout (#17)
    * Upgrade to nextjs 13 (#14)
    
    * chore: updated favicon
    
    * feat: refact layout to be more simple
    
    * style: fixed button size
    
    * fix: resume url and disable projects button
    
    * chore: upadte to nextjs 13
    
    * chore: upadte to nextjs 13
    
    * Updated layout (#15)
    
    * refact: removed unused files
    
    * feat: updated to new layout
    
    * chore: fix npm dependency security (#16)

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,7 +1,5 @@
 export const Background = () => {
   return (
-    <div className="absolute z-0 left-0 top-0 bg-[url('/grid-pattern.svg')] animate-grid h-full w-full">
-      {/* <div className="bg-[url('/waves.svg')] bg-bottom bg-no-repeat w-full h-[100%] " /> */}
-    </div>
+    <div className="absolute z-0 left-0 top-0 bg-[url('/background-pattern.svg')] animate-grid h-full w-full"></div>
   );
 };
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,52 +1,90 @@
 import Image from "next/image";
 import { useTranslation } from "next-i18next";
-import ProjectsSvg from "@/svg/projects";
-import PersonSvg from "@/svg/person";
 import Link from "next/link";
 
-export const Card = () => {
-  const { t } = useTranslation();
+const RESUME_URL = {
+  pt: "/levi-carvalho-curriculo.pdf",
+  en: "/en/levi-carvalho-resume.pdf",
+};
 
+export const Card = () => {
+  const { t, i18n } = useTranslation();
+
+  const CURRRENT_LANGUAGE = (i18n.language as "pt") || "en";
   return (
-    <div className="relative md:h-fit h-full w-full">
-      <div className="w-[130px] h-[130px] bg-[#5840BA] opacity-60 absolute left-0 top-0 blur-3xl" />
-      <div className="w-[130px] h-[130px] bg-[#5840BA] opacity-60 absolute rounded-full right-0 bottom-0 blur-3xl" />
-      <div className="flex flex-col-reverse justify-end md:flex-row md:justify-between items-center gap-10 md:gap-0  px-8 pt-10 md:pt-0 w-full h-[80vh] md:h-[350px] border border-[#242424] rounded-lg bg-[linear-gradient(to_top,rgba(15,14,17,.3),rgba(15,14,17,.1))] backdrop-blur-sm">
-        <div>
+    <div className="relative h-full w-full text-center">
+      <div className="w-[130px] h-[130px] bg-[#93BEFF] absolute left-0 top-0 blur-3xl" />
+      <div className="w-[130px] h-[130px] bg-[#7D40BA] opacity-60 absolute rounded-full right-0 bottom-0 blur-3xl" />
+      <div className="flex justify-center items-center px-8 w-full h-[80vh] rounded-lg bg-[#F5F5F574] backdrop-blur-sm">
+        <div className="flex flex-col gap-8 items-center">
           <div>
-            <p>{t("hello")} 🖖🏻</p>
-            <h1 className="text-4xl font-bold my-1">{t("i-am")} Levi</h1>
+            <p>
+              {t("hello")} 🖖🏻, {t("i-am-levi")}
+            </p>
+            <h1 className="text-5xl font-bold my-2">Fullstack Developer</h1>
             <p
               className="text-sm max-w-sm"
               dangerouslySetInnerHTML={{ __html: t("about-me") }}
             />
           </div>
-          <div className="flex my-8 gap-2">
-            <Link
-              href={t("resume-url") || "/levi-carvalho-curriculo.pdf"}
-              target="_blank"
-            >
-              <button className="w-full md:w-auto px-4 py-2 border border-gray-100 rounded flex gap-2 items-center hover:border-gray-100 hover:bg-gray-100 hover:text-black group transition-all">
-                <PersonSvg className="fill-gray-100 group-hover:fill-black" />
-                {t("my-resume")}
-              </button>
-            </Link>
-            <button
-              disabled
-              className="w-full md:w-auto px-4 py-2 border border-gray-100 rounded flex gap-2 items-center hover:border-gray-100 hover:bg-gray-100 hover:text-black group transition-all disabled:opacity-40 disabled:cursor-not-allowed"
-            >
-              <ProjectsSvg className="fill-gray-100 group-hover:fill-black" />
-              {t("projects")}
-            </button>
-          </div>
-        </div>
-        <div className="overflow-hidden rounded-full min-w-[180px] min-h-[180px] md:min-h-[250px] md:min-w-[250px] relative border border-[#242424]">
-          <Image
-            src="/profile.png"
-            alt="Profile photo"
-            fill
-            className="object-cover"
-          />
+          <ul className="flex items-center gap-8">
+            <li>
+              <Link
+                href="https://linkedin.com/in/levi-me"
+                target="_blank"
+                className="block"
+              >
+                <Image
+                  src="/linkedin.svg"
+                  alt="linkedin icon"
+                  width={24}
+                  height={24}
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://github.com/asleeplevi"
+                target="_blank"
+                className="block"
+              >
+                <Image
+                  src="/github.svg"
+                  alt="github icon"
+                  width={24}
+                  height={24}
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="mailto:jlevicarvalho@gmail.com"
+                target="_blank"
+                className="block"
+              >
+                <Image
+                  src="/email.svg"
+                  alt="email icon"
+                  width={24}
+                  height={24}
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={RESUME_URL[CURRRENT_LANGUAGE]}
+                target="_blank"
+                className="block"
+              >
+                <Image
+                  src="/paper-icon.svg"
+                  alt="email icon"
+                  width={24}
+                  height={24}
+                />
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>

5a0f6a5077eafe14a4326d3da4d445b9dda68aaf
Levi <jlevicarvalho@gmail.com>
2023-10-09 22:45:03 -0300

    feat: moved constants to a single file

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const APP_CONSTANTS = {
+  SOCIAL: {
+    GITHUB: "https://github.com/asleeplevi",
+    LINKEDIN: "https://linkedin.com/in/levi-me",
+    EMAIL: "contact@devlevi.com",
+  },
+  RESUME_URL: {
+    PT: "/levi-carvalho-curriculo.pdf",
+    EN: "/en/levi-carvalho-resume.pdf",
+  },
+};
